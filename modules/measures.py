823dd41169498983f4ac5fbb451644e13f413239
Liam Yih <ltyih@yahoo.co.uk>
2026-06-11 11:10:48 -0700

    Combined entropies into one `measure` file

--- modules/measures.py
+++ modules/measures.py
@@ -49,3 +49,17 @@ def data_counts(data_array1, data_array2):
             co2_sorted[i] = 0    
     return(co1, co2_sorted)
 
+
+def entropy(dataframe, columns = []):
+# Computes information entropy on one dataset, only includes the specified columns 
+  df = dataframe
+  if len(columns) != 0:
+        df_subset = df[columns]
+  else:
+        df_subset = df
+  array = df_subset.to_numpy()
+  arr = array.astype('U21')
+  _, counts = np.unique(arr, axis = 0,return_counts=True)    
+  probabilities = counts / len(arr)
+  dataset_entropy = -np.sum(probabilities * np.log2(probabilities))
+  return(dataset_entropy)
